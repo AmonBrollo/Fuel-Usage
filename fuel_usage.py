d2966c4a6c3188f2b49228f519a340a95618f609
Amon <77292585+AmonBrollo@users.noreply.github.com>
2021-09-28 17:58:09 -0300

    Create fuel_usage.py

---
+++ fuel_usage.py
@@ -0,0 +1,30 @@
+#file name: fuel_usage.py
+#author: Amon Brollo
+
+def main():
+    start_miles = float(input("Enter the first odometer reading (in miles): "))
+    end_miles = float(input("Enter the second odometer reading (in miles): "))
+    amount_gallons = float(input("Enter the amount of fuel used (in gallons): "))
+
+
+def miles_per_gallon(start_miles, end_miles, amount_gallons):
+    """Compute and return the average number of miles
+    that a vehicle traveled per gallon of fuel.
+    Parameters
+        start_miles: An odometer value in miles.
+        end_miles: Another odometer value in miles.
+        amount_gallons: A fuel amount in U.S. gallons.
+    Return: Fuel efficiency in miles per gallon.
+    """
+    
+    return
+
+def lp100_from_mpg(mpg):
+    """Convert miles per gallon to liters per 100
+    kilometers and return the converted value.
+    Parameter mpg: A value in miles per gallon
+    Return: The converted value in liters per 100km.
+    """
+    return
+
+    main()
